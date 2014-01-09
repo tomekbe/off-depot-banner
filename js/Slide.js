44cfab439c1303114f1ac394450dfa048d0c3827
Tomasz Bartkowiak <pixelfury@gmail.com>
2014-01-09 12:39:03 +0000

    commit on experimental branch

---
+++ js/Slide.js
@@ -0,0 +1,33 @@
+function Slide(obj) {
+	this.loaded = obj.loaded;
+	this.ready  = obj.ready;
+	this.totalToLoad = obj.totalToLoad;
+	this.path = obj.path;
+	this.images  = obj.images;
+	this.resources  = new Array();
+
+	this.setup();
+
+}
+
+
+Slide.prototype.setup = function () {
+
+	for (image in images)
+
+
+}
+
+Slide.prototype.preload   =  function () {
+	
+}
+
+
+Slide.prototype.loadingProgressCheck = function() {
+
+}
+
+Slide.prototype.drawFrame = function() {
+
+
+}
